24c1d280f373e95e1a4384cabaf24b4f23aad742
BigShotsGaming <support@bigshotzgaming.com>
2021-07-20 22:58:19 -0400

    Adding Ep 1

---
+++ Assets/Scripts/Common Practices Ep 1/CommonPractice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonPractice : MonoBehaviour {
+    #region VARIABLES
+    [Serializable]
+    public class CommonVariables {
+        public float time;
+        public bool isHappening;
+    }
+
+    public CommonVariables commVar = new CommonVariables();
+    #endregion
+
+    #region UNITY METHODS
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+    #endregion
+
+    #region PUBLIC METHODS
+    #endregion
+}
