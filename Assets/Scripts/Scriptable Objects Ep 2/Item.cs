1f666ca77e467f2baf29c15fefdc2c5af82cfc2c
BigShotsGaming <support@bigshotzgaming.com>
2021-07-22 10:18:33 -0400

    Adding Ep 2

---
+++ Assets/Scripts/Scriptable Objects Ep 2/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inventory/Items/Health")]
+public class Item : ScriptableObject {
+    public enum HealthPacks {
+        low,
+        mid,
+        high
+    }
+
+    #region VARIABLES
+    public string itemName;
+    public float itemAmountHP;
+
+    [Space]
+    public HealthPacks hp;
+    #endregion
+}
---
+++ Assets/Scripts/Scriptable Objects Ep 2/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+    #region VARIABLES
+    [SerializeField] Item item;
+    [SerializeField] float hpAmount;
+    [SerializeField] string hpName;
+    #endregion
+
+    #region UNITY METHODS
+    void Start() {
+        ShowHealth();
+    }
+
+    void Update() {
+        
+    }
+    #endregion
+
+    #region PUBLIC METHODS
+    public void ShowHealth() {
+        hpAmount = item.itemAmountHP;
+        hpName = item.itemName;
+    }
+    #endregion
+}
